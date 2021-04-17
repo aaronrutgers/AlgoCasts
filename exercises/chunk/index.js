2a0a959b4c2e6c4bb4ee18f80633b462c85859c2
Can Li <>
2021-04-16 20:44:40 -0400

    1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let index = 0
+    const chunked = []
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size))
+        index += size
+    }
+    return chunked
+}
 
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//     const chunked = []
+//     for (element of array) {
+//         let last  = chunked[chunked.length - 1]
+//         if (!last || last.length === size) {
+//             chunked.push([element])
+//         } else {
+//             last.push(element)
+//         }
+//     }
+//     return chunked
+// }
