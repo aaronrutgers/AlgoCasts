47260131968fbea16b18440bfb5138e6e60bfc82
aaronrutgers <aaron.now.coder@gmail.com>
2021-04-23 23:04:11 -0400

    1

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  map = {}
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.map[eventName]) {
+      this.map[eventName].push(callback)
+    } else {
+      this.map[eventName] = [callback]
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    this.map[eventName]?.forEach(callback => {
+      callback()
+    });
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.map[eventName]
+  }
 }
 
 module.exports = Events;
